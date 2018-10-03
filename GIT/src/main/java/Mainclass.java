0970128cfc456d2ce7817975ed0e9a194babf7c9
Hana.Hefny <Hana.Hefny@DEV09.nm-eg.com>
2018-10-03 10:07:33 +0200

    push project

---
+++ GIT/src/main/java/Mainclass.java
@@ -0,0 +1,16 @@
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sales data=new Sales();
+		displayGreeting();
+		data.display();
+
+	
+	}
+	private static void displayGreeting() {
+		System.out.println("hello");
+	}
+
+}
---
+++ GIT/src/main/java/Sales.java
@@ -0,0 +1,9 @@
+
+public class Sales {
+
+	int data[] = { 0, 2, 4 };
+
+	public void display() {
+		System.out.println("hello sales");
+	}
+}
